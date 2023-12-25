edfa15331704d19bf65f5bbfc98027567276e94a
Wasif Bin Zahir <wasifbinzahir16@gmail.com>
2023-12-25 22:38:48 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,229 @@
+
+<?php include 'header.php';?>
+
+<body>
+
+
+
+    <?php include 'menu.php';?>
+
+
+    <!-- slider_area_start -->
+    <div class="slider_area">
+        <div class="single_slider  d-flex align-items-center slider_bg_1">
+            <div class="container">
+                <div class="row align-items-center justify-content-center" style="margin-bottom: 200px;">
+                    <div class="col-xl-8 ">
+                        <div class="slider_text text-center">
+                            <div class="text">
+                                <h3 style="color: red;margin-bottom: 100px;">
+                                    Eat Sleep Repeat
+                                </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    <!-- slider_area_end -->
+    <!-- recepie_area_start  -->
+    <div class="recepie_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="single_recepie text-center">
+                        <div class="recepie_thumb">
+                            <img src="img/recepie/recpie_1.png" alt="">
+                        </div>
+                        <h3>Khicuri</h3>
+                        <span>Main Course</span>
+                        <p>Time Needs: 30 Mins</p>
+                        <a href="#" class="line_btn">View Full Recipe</a>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="single_recepie text-center">
+                        <div class="recepie_thumb">
+                            <img src="img/recepie/recpie_2.png" alt="">
+                        </div>
+                        <h3>Aloo Parata</h3>
+                        <span>Main Meal</span>
+                        <p>Time Needs: 30 Mins</p>
+                        <a href="#" class="line_btn">View Full Recipe</a>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="single_recepie text-center">
+                        <div class="recepie_thumb">
+                            <img src="img/recepie/recpie_3.png" alt="">
+                        </div>
+                        <h3>Beef Kala Bhuna</h3>
+                        <span>Side Dish</span>
+                        <p>Time Needs: 30 Mins</p>
+                        <a href="#" class="line_btn">View Full Recipe</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /recepie_area_start  -->
+
+    <!-- recepie_videos   -->
+    <div class="recepie_videoes_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-6">
+                    <div class="recepie_info">
+                        <h3>Learn about different recipes with step by step procedures</h3>
+                    <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women.</p>
+                   
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="videos_thumb">
+                        <div class="big_img">
+                            <img src="img/video/big.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ recepie_videos   -->
+
+    <!-- dish_area start  -->
+    <div class="dish_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="dish_wrap d-flex">
+                        <div class="single_dish text-center">
+                            <div class="thumb">
+                                <img src="img/recepie/bd.png" style="height: 200px; width: 200px; border-radius: 100px;" alt="">
+                            </div>
+                            <h3>Bangladeshi cuisine</h3>
+                            <p>Bangladeshi cuisine is the national cuisine of Bangladesh. It has been shaped by the region's history and river-line geography. The country has a tropical monsoon climate. The staple of Bangladesh is rice and fish.</p>
+                        </div>
+                        <div class="single_dish text-center">
+                            <div class="thumb">
+                                <img src="img/recepie/in.png"style="height: 200px; width: 200px; border-radius: 100px;" alt="">
+                            </div>
+                            <h3>Indian cuisine</h3>
+                            <p>Indian cuisine consists of a variety of regional and traditional cuisines native to India. Given the diversity in soil, climate, culture, ethnic groups, and occupations, these cuisines vary substantially and use locally available spices, herbs, vegetables, and fruits.</p>
+                        </div>
+                        <div class="single_dish text-center">
+                            <div class="thumb">
+                                <img src="img/recepie/cn.png" style="height: 200px; width: 200px; border-radius: 100px;"alt="">
+                            </div>
+                            <h3>Chinese cuisine</h3>
+                            <p>Chinese cuisine comprise cuisines originating from China. Because of the Chinese diaspora and historical power of the country, Chinese cuisine has profoundly influenced many other cuisines in Asia and beyond, with modifications made to cater to local palates. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ dish_area start  -->
+
+    <!-- latest_trand     -->
+    <div class="latest_trand_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="trand_info text-center">
+                        <p>Thousands of recipes are waiting to be watched</p>
+                        <h3>Discover latest trending recipes</h3>
+                        <a href="#" class="boxed-btn3">View all Recipes</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ latest_trand     -->
+
+    <!-- customer_feedback_area  -->
+    <div class="customer_feedback_area">
+        <div class="container">
+            <div class="row justify-content-center mb-50">
+                <div class="col-xl-9">
+                    <div class="section_title text-center">
+                        <h3>Feedback From Customers</h3>
+                        <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially <br> in the workplace. That’s why it’s crucial that, as women.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="customer_active owl-carousel">
+                        <div class="single_customer d-flex">
+                            <div class="thumb">
+                                <img src="img/testmonial/sultansir.png" style="height: 100px; width: 100px; border-radius: 10px;" alt="">
+                            </div>
+                            <div class="customer_meta">
+                                <h3>Mohd. Sultan Ahammad</h3>
+                                <span>Supervisor</span>
+                                <p>You're had. Subdue grass Meat us winged years you'll doesn't. fruit two also won one yielding creepeth third give may never lie alternet food.</p>
+                            </div>
+                        </div>
+                        <div class="single_customer d-flex">
+                            <div class="thumb">
+                                <img src="img/testmonial/wasif.png" style="height: 100px; width: 100px; border-radius: 10px;"  alt="">
+                            </div>
+                            <div class="customer_meta">
+                                <h3>Wasif Bin Zahir</h3>
+                                <span>Developer</span>
+                                <p>You're had. Subdue grass Meat us winged years you'll doesn't. fruit two also won one yielding creepeth third give may never lie alternet food.</p>
+                            </div>
+                        </div>
+                        <div class="single_customer d-flex">
+                            <div class="thumb">
+                                <img src="img/testmonial/miraj.png" style="height: 100px; width: 100px; border-radius: 10px;" alt="">
+                            </div>
+                            <div class="customer_meta">
+                                <h3>Nafiul Alam Miraj</h3>
+                                <span>Developer</span>
+                                <p>You're had. Subdue grass Meat us winged years you'll doesn't. fruit two also won one yielding creepeth third give may never lie alternet food.</p>
+                            </div>
+                        </div>
+                        <div class="single_customer d-flex">
+                            <div class="thumb">
+                                <img src="img/testmonial/rahat.png"style="height: 100px; width: 100px; border-radius: 10px;"  alt="">
+                            </div>
+                            <div class="customer_meta">
+                                <h3>Fazle Rabbi Rahat</h3>
+                                <span>Developer</span>
+                                <p>You're had. Subdue grass Meat us winged years you'll doesn't. fruit two also won one yielding creepeth third give may never lie alternet food.</p>
+                            </div>
+                        </div>
+                        <div class="single_customer d-flex">
+                            <div class="thumb">
+                                <img src="img/testmonial/nazir.png"style="height: 100px; width: 100px; border-radius: 10px;"  alt="">
+                            </div>
+                            <div class="customer_meta">
+                                <h3>Nazir Ahmed Prince</h3>
+                                <span>Developer</span>
+                                <p>You're had. Subdue grass Meat us winged years you'll doesn't. fruit two also won one yielding creepeth third give may never lie alternet food.</p>
+                            </div>
+                        </div>
+                        <div class="single_customer d-flex">
+                            <div class="thumb">
+                                <img src="img/testmonial/shamim.png"style="height: 100px; width: 100px; border-radius: 10px;"  alt="">
+                            </div>
+                            <div class="customer_meta">
+                                <h3>Md Shamim</h3>
+                                <span>Developer</span>
+                                <p>You're had. Subdue grass Meat us winged years you'll doesn't. fruit two also won one yielding creepeth third give may never lie alternet food.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- / customer_feedback_area  -->
+
+    <?php include 'footer.php';?>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,51 @@
+ <!-- header-start -->
+ <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area ">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-3 col-lg-2">
+                            <div class="logo">
+                                <a href="index.html">
+                                    <img src="" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-7">
+                            <div class="main-menu   d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="about.html">About Us</a></li>
+                                        <li><a href="Recipes.html">Recipes</a></li>
+                                        <!-- <li><a href="#">Receipe Article<i class="ti-angle-down"></i></a>
+                                            <ul class="submenu">
+                                                <li><a href="blog.html">blog</a></li>
+                                                <li><a href="single-blog.html">single-blog</a></li>
+                                            </ul>
+                                        </li> -->
+                                        
+                                        <!-- <li>
+                                            <a href="#">pages <i class="ti-angle-down"></i></a>
+                                            <ul class="submenu">
+                                                <li><a href="recipes_details.html">Recipes Details</a></li>
+                                             <li><a href="elements.html">elements</a></li> 
+                                            </ul>
+                                        </li> -->
+
+                                        <li><a href="contact.html">Contact Us</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                       
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
